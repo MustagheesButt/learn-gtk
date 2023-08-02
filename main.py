9fed599208fa3e45dfdd323fabe90afe35c2db66
Mustaghees Butt <renaldpirate@gmail.com>
2023-08-02 19:35:54 +0000

    playing with grid

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw
+import os
+import pathlib
 
 
 class MyApp(Adw.Application):
@@ -15,16 +17,40 @@ class MyApp(Adw.Application):
         builder.add_from_file("main.ui")
 
         # Obtain the button widget and connect it to a function
-        button = builder.get_object("btn1")
-        button.connect("clicked", self.hello)
+        #button = builder.get_object("btn1")
+        #button.connect("clicked", self.hello)
+        grid = builder.get_object("grid1")
 
         # Obtain and show the main window
         self.win = builder.get_object("main_window")
         self.win.set_application(self)  # Application will close once it no longer has active windows attached to it
         self.win.present()
 
-    def hello(self, button):
-        print("Hello world!")
+        media_files = self.get_media_files()
+        self.add_media_to_grid(grid, media_files)
+
+    def get_media_files(self):
+        #exts = [".3g2", ".3gp", ".asf", ".asx", ".avi", ".flv", \
+        #                ".m2ts", ".mkv", ".mov", ".mp4", ".mpg", ".mpeg", \
+        #                ".rm", ".swf", ".vob", ".wmv"]
+        exts = [".mkv"]
+        found = []
+        for root, dirs, files in os.walk("/home"):
+            for file in files:
+                if file.endswith(tuple(exts)):
+                    found.append(os.path.join(root, file))
+
+        return found
+
+    def add_media_to_grid(self, grid, files):
+        for i in range(len(files[:3])):
+            file = files[i]
+            col = i % 3
+            row = int(i / 3)
+            print(col, row)
+
+            btn = Gtk.Button(label=file)
+            grid.attach(btn, col * 400, row, 100, 100)
 
 app = MyApp(application_id="com.example.GtkApplication")
 
